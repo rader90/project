6b3e6bbff9f2c93146c2b5eca70b13ca76ea5bef
rader90 <13xr@mail.ru>
2020-04-16 17:44:38 +0500

    IT works!

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define IT_WORKS "It works!"
+using namespace std;
+
+int main(int argc, char** args) 
+{
+	const int SUCCESS = 0;	
+	cout << IT_WORKS << endl;
+	return SUCCESS;
+}	
