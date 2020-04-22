bda0a45dcd9c9bf51c707e1a9edc7d5aef95e0fa
rader90 <13xr@mail.ru>
2020-04-22 14:02:13 +0500

    dz

--- include/program.h
+++ include/program.h
@@ -1,4 +1,4 @@
-//Подключение стандартной библиотеки
 #include <iostream> 
-//Подстановочная константа
+
 #define IT_WORKS "\tSample string\n\tSample string 2" 
+#define HI "\n \rHello friend!"
--- src/program.cpp
+++ src/program.cpp
@@ -1,16 +1,12 @@
 #include "program.h"
 
-// Разрешение использования имен из стандартной библиотеки
 using namespace std; 
 
-//Внутренние имена
 int main(int argc, char** args) 
 {
-	//имя константы
 	const int SUCCESS = 0;
-		//обращение к стандартной библиотеки
 	cout << IT_WORKS << endl;
-	//тестирование команды system
-	system("pause");
+	cout << HI << endl;
+	
 	return SUCCESS;
-}	
+}
