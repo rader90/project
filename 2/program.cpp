955b8c6acb7ac578946f49ca323f789fc94429d8
rader90 <13xr@mail.ru>
2020-04-23 17:46:08 +0500

    лекции 2

---
+++ 2/program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char** args)
+{
+	short a;
+	unsigned char b;
+	a=45000;
+	//b=1400;
+	cout << a;
+	//cout << b;
+	//cout << "a = "<< a <<"\n b="<<b << endl; 
+	return 0;	
+}
