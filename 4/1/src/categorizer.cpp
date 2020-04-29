fd862e79f1b3dbb3afbf5b373a04375d94cf931a
rader90 <13xr@mail.ru>
2020-04-29 23:48:45 +0500

    up 4.2

--- 4/1/src/categorizer.cpp
+++ 4/1/src/categorizer.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
+unsigned int histogram[256];
+int alphabet[256];
+
 int main(int argc, char** args)
 {
 	fstream f;
+	int width, height;
+	width = 0;
+	height = 0;
+	char PGM_hdr1[2];
 	if(argc == 1){
 		cout << "Please specify input file(s)!" << endl;
 		return 0;
@@ -14,9 +21,32 @@ int main(int argc, char** args)
 		if(!f.is_open())
 		{
 			cout << "File(s)" << args[i] << "is not accessible!" << endl;
-			return 0;
+			continue;
 		}
+		cout << "File " << args[i] << " opened" << endl;
+		f.red(PGM_hdr1,2);
+		if((PGM_hdr1[0] != 0x50) || (PGM_hdr1[1] != 0x35)) continue;
+		cout << "PGM header detected" << endl;
+		f.seekg(static_cast<unsigned int>(f.tellg())+1);
+		unsigned int digit_b = static_cast<unsigned int>(f.tellg());
+		char sym
+		do
+		{
+			f.read(&sym,1)
+		}while(sym != 0x20);
+		unsigned int digit_e = static_cast<unsigned int>(f.tellg()) - 2;
+		int j = 0;
+		for(int d = '0'; d <= '9'; d++, j++) alphabet[d] = j;
+		int c = 1;
+		for(j = digit_e; j <= digit_b; j--, c *= 10)
+		{
+			f.seekg(j);
+			f.read(&sym, 1);
+			width += alphabet[sym] * c;
+		}
+		cout << "Width: " << width << endl;
 		f.close();
+		
 	}
 	return 0;
 }
---
+++ 4/build-1-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/CMakeFiles/categorizer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/categorizer.dir/src/categorizer.cpp.obj"
+  "categorizer.exe"
+  "categorizer.exe.manifest"
+  "categorizer.pdb"
+  "libcategorizer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/categorizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
