372a951fe83d4a5da2c4f232ee8d8b65b413e825
rader90 <13xr@mail.ru>
2020-09-07 17:59:39 +0500

    new program

---
+++ Progs/1/program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	system("chcp 1251");
+	cout << "Privet!" << endl;
+	cout << "Kak dela?" << endl;
+	cout << "Как дела?" << endl;
+	system("pause");
+}
